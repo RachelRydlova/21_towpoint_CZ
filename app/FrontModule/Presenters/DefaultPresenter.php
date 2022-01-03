f9b6d7d91b4f76c551c59e9fa9520cdc76a85a04
rachelrydlova <rachel.rydlova@vapol.cz>
2022-01-03 12:47:08 +0100

    facebook pixel, test

--- app/FrontModule/Presenters/DefaultPresenter.php
+++ app/FrontModule/Presenters/DefaultPresenter.php
@@ -171,7 +171,27 @@ class DefaultPresenter extends BasePresenter
 
 
     public function renderDefault(){
-        // data pro banner
+        //facebook
+        $fbpixel = "
+        <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '217382757240771');
+fbq('track', 'PageView');
+</script>
+<noscript><img height=\"1\" width=\"1\" style=\"display:none\"
+src=\"https://www.facebook.com/tr?id=217382757240771&ev=PageView&noscript=1\"
+    /></noscript>
+        ";
+        $this->template->pixel = $fbpixel;
+
+     // data pro banner
         $b = $this->bannerModel->getBannerData();
         $this->template->b = $b[0];
         $this->template->format = $b[1];
