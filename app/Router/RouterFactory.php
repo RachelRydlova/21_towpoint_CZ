c1ddc0fff3e5887396d1edcabb63eacee76ee70f
rachelrydlova <rachel.rydlova@vapol.cz>
2022-02-08 08:40:26 +0100

    edit routy

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -36,6 +36,7 @@ final class RouterFactory
         $router[] = new Nette\Application\Routers\Route('/clanek-montaz', 'Front:Blog:montaz');
         $router[] = new Nette\Application\Routers\Route('/clanek-pece-o-tazne', 'Front:Blog:peceOTazne');
         $router[] = new Nette\Application\Routers\Route('/clanek-tazne-hybrid', 'Front:Blog:tazneHybrid');
+        $router[] = new Nette\Application\Routers\Route('/proc-my', 'Front:Blog:procNas');
 
         $router[] = new Nette\Application\Routers\Route('/akce-souteze', 'Front:Default:akce');
         $router[] = new Nette\Application\Routers\Route('/tisicovkaProTebe', 'Front:Default:akce1000montaz');
