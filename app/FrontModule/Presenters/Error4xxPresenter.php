2c7d1e1310f71f5fc5b8afbb2fffdeca3b72b50b
Rachel Rydlova <rachel.kovarickova@vapol.eu>
2022-04-20 13:14:27 +0200

    edit obrazky nosice

--- app/FrontModule/Presenters/Error4xxPresenter.php
+++ app/FrontModule/Presenters/Error4xxPresenter.php
@@ -2,28 +2,28 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\FrontModule\Presenters;
 
 use Nette;
 
 
 final class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
-	public function startup(): void
-	{
-		parent::startup();
-		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
-			$this->error();
-		}
-	}
+    public function startup(): void
+    {
+        parent::startup();
+        if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+            $this->error();
+        }
+    }
 
 
-	public function renderDefault(Nette\Application\BadRequestException $exception): void
-	{
+    public function renderDefault(Nette\Application\BadRequestException $exception): void
+    {
         $url = $this->getHttpRequest()->getUrl();
         $this->template->url = $url;
-		// load template 403.latte or 404.latte or ... 4xx.latte
-		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
-	}
+        // load template 403.latte or 404.latte or ... 4xx.latte
+        $file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
+        $this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+    }
 }
